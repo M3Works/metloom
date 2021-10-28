03309ba43f60c3181fa70348510bab1dffd5f640
Micah Sandusky <micc5725@gmail.com>
2021-10-28 16:34:26 -0600

    flake8

--- tests/test_dataframe_utils.py
+++ tests/test_dataframe_utils.py
@@ -7,7 +7,9 @@ from metloom.dataframe_utils import join_df, append_df, merge_df
 
 df1 = pd.DataFrame.from_records([{"foo": 12.0}, {"foo": 11.0}])
 df2 = pd.DataFrame.from_records([{"bar": 1.0}, {"bar": 1.0}])
-df3 = pd.DataFrame.from_records([{"foo": 12.0, "bar": 1.0}, {"foo": 11.0, "bar": 1.0}])
+df3 = pd.DataFrame.from_records([
+    {"foo": 12.0, "bar": 1.0}, {"foo": 11.0, "bar": 1.0}
+])
 df4 = pd.DataFrame.from_records(
     [
         {"foo": 12.0},
@@ -41,14 +43,18 @@ def test_append_df(set1, set2, expected):
 def test_merge_df():
     first = pd.DataFrame(
         {
-            "datetime": [datetime(2020, 1, 3), datetime(2020, 1, 6), datetime(2020, 1, 7)],
+            "datetime": [
+                datetime(2020, 1, 3), datetime(2020, 1, 6),
+                datetime(2020, 1, 7)
+            ],
             "a": [3.0, 6.0, 7.0],
             "c": [3.0, 6.0, 7.0]
         }
     ).set_index("datetime")
     second = pd.DataFrame(
         {
-            "datetime": [datetime(2020, 1, 2), datetime(2020, 1, 5),
+            "datetime": [
+                datetime(2020, 1, 2), datetime(2020, 1, 5),
                 datetime(2020, 1, 6), datetime(2020, 1, 8)],
             "b": [2.0, 5.0, 6.0, 8.0],
             "c": [2.0, 5.0, 6.0, 8.0]
@@ -57,7 +63,8 @@ def test_merge_df():
 
     expected = pd.DataFrame(
         {
-            "datetime": [datetime(2020, 1, 2), datetime(2020, 1, 3), datetime(2020, 1, 5),
+            "datetime": [
+                datetime(2020, 1, 2), datetime(2020, 1, 3), datetime(2020, 1, 5),
                 datetime(2020, 1, 6), datetime(2020, 1, 7), datetime(2020, 1, 8)],
             "a": [np.nan, 3.0, np.nan, 6.0, 7.0, np.nan],
             "c": [2.0, 3.0, 5.0, 6.0, 7.0, 8.0],
