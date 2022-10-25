9716bb1b15ac99abcf75ff93251f19b5ce0d5501
Micah Sandusky <micc5725@gmail.com>
2022-10-25 11:21:57 -0600

    change mock for meta

--- tests/test_usgs.py
+++ tests/test_usgs.py
@@ -1,4 +1,6 @@
 from datetime import timezone, timedelta, datetime
+from os.path import join
+from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 import geopandas as gpd
@@ -8,6 +10,9 @@ import pytest
 from metloom.pointdata import USGSPointData
 from metloom.variables import USGSVariables
 from tests.test_point_data import BasePointDataTest
+
+
+DATA_DIR = str(Path(__file__).parent.joinpath("data"))
 
 
 class TestUSGSStation(BasePointDataTest):
@@ -42,18 +47,18 @@ class TestUSGSStation(BasePointDataTest):
 
     @pytest.fixture(scope="class")
     def crp_daily_expected(self):
-        points = gpd.points_from_xy([-106.54], [37.35])
+        points = gpd.points_from_xy([-106.54], [37.35], z=[1000.0])
         df = gpd.GeoDataFrame.from_dict(
             [
                 {
-                    "datetime": pd.Timestamp("2020-07-01 05:00:00+0000", tz="UTC"),
+                    "datetime": pd.Timestamp("2020-07-01 07:00:00+0000", tz="UTC"),
                     "DISCHARGE": '111',
                     "DISCHARGE_units": "cf/s",
                     "site": "08245000",
                     "datasource": "USGS"
                 },
                 {
-                    "datetime": pd.Timestamp("2020-07-02 05:00:00+0000", tz="UTC"),
+                    "datetime": pd.Timestamp("2020-07-02 07:00:00+0000", tz="UTC"),
                     "DISCHARGE": '112',
                     "DISCHARGE_units": "cf/s",
                     "site": "08245000",
@@ -79,26 +84,10 @@ class TestUSGSStation(BasePointDataTest):
 
     @staticmethod
     def crp_meta_return():
-        return {
-            "value": {
-                "timeSeries": [
-                    {
-                        "values": [
-                            {"value": [
-                                {"value": '111', "dateTime": "2020-07-01T00:00:00.000"},
-                                {"value": '112', "dateTime": "2020-07-02T00:00:00.000"}
-                            ]}],
-                        "sourceInfo": {
-                            "geoLocation": {
-                                "geogLocation":
-                                    {"longitude": -106.54, "latitude": 37.35}
-                            },
-                            "timeZoneInfo": {"defaultTimeZone": {"zoneOffset": "-5:00"}}
-                        },
-                        "variable": {"unit": {"unitCode": "cf/s"},
-                                     "noDataValue": -9999.0},
-                    }]}
-        }
+        with open(join(DATA_DIR, "platoro_meta.txt")) as fp:
+            data_text = fp.read()
+
+        return data_text
 
     @classmethod
     def crp_side_effect(cls, url, **kwargs):
@@ -106,16 +95,13 @@ class TestUSGSStation(BasePointDataTest):
         params = kwargs["params"]
 
         if "startDT" not in params:
-            mock.json.return_value = cls.crp_meta_return()
+            mock.text = cls.crp_meta_return()
         elif "startDT" in params:
             mock.json.return_value = cls.usgs_daily_discharge_response()
         else:
             raise ValueError("unknown scenario")
 
         return mock
-
-    def test_class_variables(self):
-        assert USGSPointData("no", "no").tzinfo == timezone(timedelta(hours=0.0))
 
     def test_get_metadata(self, crp_station):
 
@@ -125,16 +111,16 @@ class TestUSGSStation(BasePointDataTest):
             mock_get = mock_requests.get
             assert mock_get.call_count == 1
             mock_get.assert_called_with(
-                "https://waterservices.usgs.gov/nwis/dv/",
+                "https://waterservices.usgs.gov/nwis/site/",
                 params={
-                    'sites': '08245000',
-                    'format': 'json',
-                    'siteType': 'ST',
-                    'siteStatus': 'all'
+                    "format": "rdb",
+                    "sites": "08245000",
+                    "siteOutput": "expanded",
+                    "siteStatus": "all"
                 },
             )
 
-        expected = gpd.points_from_xy([-106.54], [37.35])[0]
+        expected = gpd.points_from_xy([-106.54], [37.35], z=[1000.0])[0]
         assert expected == metadata
 
     def test_get_daily_data(self, crp_station, crp_daily_expected):
