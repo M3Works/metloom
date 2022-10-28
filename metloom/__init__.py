0dda6cf92eb038f9b58dc1496f8aaaa48102decf
Micah Sandusky <micc5725@gmail.com>
2022-10-28 14:09:44 -0600

    Bump version: 0.2.14 → 0.3.0

--- metloom/__init__.py
+++ metloom/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """M3Works"""
 __email__ = "m3worksllc@gmail.com"
-__version__ = '0.2.14'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/M3Works/metloom',
-    version='0.2.14',
+    version='0.3.0',
     zip_safe=False,
 )
