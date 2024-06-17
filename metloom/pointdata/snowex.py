42ce4badfc21f2800a36497345903e292af02c5a
micah johnson <micah.johnson150@gmail.com>
2024-06-17 13:27:59 -0600

    Flaked!

--- metloom/pointdata/snowex.py
+++ metloom/pointdata/snowex.py
@@ -1,12 +1,7 @@
-from datetime import datetime, timezone, timedelta
-from typing import List
 import logging
-import geopandas as gpd
 import pandas as pd
-from enum import Enum
 from metloom.pointdata import CSVPointData, StationInfo
 from metloom.variables import SnowExVariables
-from pathlib import Path
 import os
 
 
@@ -15,11 +10,16 @@ LOG = logging.getLogger(__name__)
 
 class SnowExMetInfo(StationInfo):
     # Name, id, lat, long, elevation, http filename
-    GM_STUDY_PLOT = "Grand Mesa Study Plot", "GMSP",  39.05084, -108.06144, 10626, "2017.06.21/SNEX_Met_GMSP2_final_output.csv"
-    LS_OBS_SITE = "Local Scale Observation Site", "LSOS", 39.05225, -108.09792, 9791, "2016.10.09/SNEX_Met_LSOS_final_output.csv"
-    MESA_EAST = "Mesa East", "ME", 39.10358, -107.88383, 10105, "2016.10.10/SNEX_Met_ME_final_output.csv"
-    MESA_MIDDLE = "Mesa Middle", "MM", 39.03954, -107.94174, 10286, "2016.10.10/SNEX_Met_MM_final_output.csv"
-    MESA_WEST = "Mesa West", "MW",  39.03388, -108.21399, 9950, "2016.10.09/SNEX_Met_MW_final_output.csv"
+    GM_STUDY_PLOT = ("Grand Mesa Study Plot", "GMSP", 39.05084, -108.06144, 10626,
+                     "2017.06.21/SNEX_Met_GMSP2_final_output.csv")
+    LS_OBS_SITE = ("Local Scale Observation Site", "LSOS", 39.05225, -108.09792, 9791,
+                   "2016.10.09/SNEX_Met_LSOS_final_output.csv")
+    MESA_EAST = ("Mesa East", "ME", 39.10358, -107.88383, 10105,
+                 "2016.10.10/SNEX_Met_ME_final_output.csv")
+    MESA_MIDDLE = ("Mesa Middle", "MM", 39.03954, -107.94174, 10286,
+                   "2016.10.10/SNEX_Met_MM_final_output.csv")
+    MESA_WEST = ("Mesa West", "MW", 39.03388, -108.21399, 9950,
+                 "2016.10.09/SNEX_Met_MW_final_output.csv")
 
 
 class SnowExMet(CSVPointData):
@@ -28,6 +28,7 @@ class SnowExMet(CSVPointData):
     """
     ALLOWED_VARIABLES = SnowExVariables
     ALLOWED_STATIONS = SnowExMetInfo
+
     # Data is in UTC
     UTC_OFFSET_HOURS = 0
 
