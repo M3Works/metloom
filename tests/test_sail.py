7463884195878516c1f863f0da437ae5661e6bd2
Andrew E Slaughter <slaughter98@gmail.com>
2025-05-07 17:23:47 -0600

    Provide limits on start/end times that align with what is available

--- tests/test_sail.py
+++ tests/test_sail.py
@@ -1,6 +1,7 @@
-
+from datetime import datetime
 from metloom.pointdata.sail import SAILPointData
 from metloom.variables import SAILStationVariables
+import pytest
 
 def test_get_hourly_data():
     obj = SAILPointData()
@@ -17,3 +18,15 @@ def test_get_daily_data():
     assert len(df) == 2
     assert df.iloc[1]["PRECIPITATION"] == 6.27
     assert df.iloc[1]["PRECIPITATION_units"] == "mm"
+
+def test_check_start_end_dates():
+    obj = SAILPointData()
+    # Test with end_date before start_date
+    with pytest.raises(ValueError):
+        obj._check_start_end_dates(datetime.fromisoformat("2023-01-02"), datetime.fromisoformat( "2023-01-01"))
+    # Test with start_date before 2021-09-01
+    with pytest.raises(ValueError):
+        obj._check_start_end_dates(datetime.fromisoformat("2020-09-01"), datetime.fromisoformat("2023-01-01"))
+    # Test with end_date after after 2023-06-16
+    with pytest.raises(ValueError):
+        obj._check_start_end_dates(datetime.fromisoformat("2023-01-01"), datetime.fromisoformat("2024-01-01"))
