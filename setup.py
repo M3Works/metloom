6f738eecedc865b6065016881c9da5e73208e131
Micah Sandusky <micc5725@gmail.com>
2026-07-30 10:01:06 -0600

    Bump version: 0.9.4 → 0.10.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/M3Works/metloom',
-    version='0.9.4',
+    version='0.10.0',
     zip_safe=False,
 )
